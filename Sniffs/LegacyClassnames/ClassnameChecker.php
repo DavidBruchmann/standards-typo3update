06b46baeab79207703f7b20d15621083771084a3
Daniel Siepmann <coding@daniel-siepmann.de>
2017-03-07 14:53:31 +0100

    FEATURE: Also migrate PHPDoc annotations
    * Migrate @param and @return statements.

---
+++ Sniffs/LegacyClassnames/ClassnameChecker.php
@@ -0,0 +1,108 @@
+<?php
+namespace Typo3Update\Sniffs\LegacyClassnames;
+
+/*
+ * Copyright (C) 2017  Daniel Siepmann <coding@daniel-siepmann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use PHP_CodeSniffer_File as PhpcsFile;
+
+/**
+ * Provide common uses for all sniffs.
+ */
+trait ClassnameChecker
+{
+    /**
+     * Contains mapping from old -> new class names.
+     * @var array
+     */
+    protected $legacyClassnames = [];
+
+    /**
+     * @param string $mappingFile File containing php array for mapping.
+     */
+    public function initialize($mappingFile = __DIR__ . '/../../../../../LegacyClassnames.php')
+    {
+        if ($this->legacyClassnames !== []) {
+            return;
+        }
+
+        $legacyClassnames = require $mappingFile;
+        $this->legacyClassnames = $legacyClassnames['aliasToClassNameMapping'];
+    }
+
+    /**
+     * @param string $classname
+     * @return bool
+     */
+    public function isLegacyClassname($classname)
+    {
+        $this->initialize();
+        return isset($this->legacyClassnames[strtolower($classname)]);
+    }
+
+    /**
+     * @param string $classname
+     * @return string
+     */
+    public function getNewClassname($classname)
+    {
+        $this->initialize();
+        return $this->legacyClassnames[strtolower($classname)];
+    }
+
+    /**
+     * Add an fixable error if given $classname is legacy.
+     *
+     * @param PhpcsFile $phpcsFile
+     * @param int $classnamePosition
+     * @param string $classname
+     */
+    public function addFixableError(PhpcsFile $phpcsFile, $classnamePosition, $classname)
+    {
+        if ($this->isLegacyClassname($classname) === false) {
+            return;
+        }
+
+        $fix = $phpcsFile->addFixableError(
+            'Legacy classes are not allowed; found %s',
+            $classnamePosition,
+            'legacyClassname',
+            [$classname]
+        );
+
+        if ($fix === true) {
+            $phpcsFile->fixer->replaceToken(
+                $classnamePosition,
+                $this->getTokenForReplacement('\\' . $this->getNewClassname($classname))
+            );
+        }
+    }
+
+    /**
+     * String to use for replacing / fixing the token.
+     * Default is class name itself, can be overwritten in sniff for special behaviour.
+     *
+     * @param string $classname
+     * @return string
+     */
+    public function getTokenForReplacement($classname)
+    {
+        return $classname;
+    }
+}
---
+++ Sniffs/LegacyClassnames/DocCommentSniff.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * Copyright (C) 2017  Daniel Siepmann <coding@daniel-siepmann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * Migrate PHP Doc comments.
+ *
+ * E.g. annotations like @param or @return, see $allowedTags.
+ */
+class Typo3Update_Sniffs_LegacyClassnames_DocCommentSniff implements PHP_CodeSniffer_Sniff
+{
+    use \Typo3Update\Sniffs\LegacyClassnames\ClassnameChecker;
+
+    /**
+     * The configured tags will be processed.
+     * @var array<string>
+     */
+    protected $allowedTags = ['@param', '@return'];
+
+    /**
+     * Original token for reuse accross methods.
+     * @var array
+     */
+    protected $originalToken = [];
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array<int>
+     */
+    public function register()
+    {
+        return [
+            T_DOC_COMMENT_TAG,
+        ];
+    }
+
+    /**
+     * Processes the tokens that this sniff is interested in.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where the token was found.
+     * @param int                  $stackPtr  The position in the stack where
+     *                                        the token was found.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        if (!in_array($tokens[$stackPtr]['content'], $this->allowedTags)) {
+            return;
+        }
+        $classnamePosition = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $stackPtr);
+        if ($classnamePosition === false) {
+            return;
+        }
+        $classname = explode(' ', $tokens[$classnamePosition]['content'])[0];
+
+        $this->originalToken = $tokens[$classnamePosition]['content'];
+        $this->addFixableError($phpcsFile, $classnamePosition, $classname);
+    }
+
+    /**
+     * As token contains more then just class name, we have to build new content ourself.
+     *
+     *
+     * @param string $classname
+     * @return string
+     */
+    public function getTokenForReplacement($classname)
+    {
+        $token = explode(' ', $this->originalToken);
+        $token[0] = $classname;
+
+        return implode(' ', $token);
+    }
+}
