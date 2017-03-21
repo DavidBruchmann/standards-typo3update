a457efa491832821d41cc6f2013d99dcdd1d1592
Daniel Siepmann <coding@daniel-siepmann.de>
2017-03-21 09:14:11 +0100

    FEATURE: Provide sniff to migrate is_a calls
    Resolves: #22

---
+++ Sniffs/LegacyClassnames/IsACallSniff.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * Copyright (C) 2017  Daniel Siepmann <coding@daniel-siepmann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use PHP_CodeSniffer_Tokens as Tokens;
+
+/**
+ * Detect and migrate instantiations of old legacy classnames using "makeInstance".
+ */
+class Typo3Update_Sniffs_LegacyClassnames_IsACallSniff implements PHP_CodeSniffer_Sniff
+{
+    use \Typo3Update\Sniffs\LegacyClassnames\ClassnameCheckerTrait;
+    use \Typo3Update\Sniffs\ExtendedPhpCsSupportTrait;
+
+    /**
+     * Original token content for reuse accross methods.
+     * @var string
+     */
+    protected $originalTokenContent = '';
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array<int>
+     */
+    public function register()
+    {
+        return [T_STRING];
+    }
+
+    /**
+     * Processes the tokens that this sniff is interested in.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where the token was found.
+     * @param int                  $stackPtr  The position in the stack where
+     *                                        the token was found.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        if (!$this->isFunctionCall($phpcsFile, $stackPtr)) {
+            return;
+        }
+        $tokens = $phpcsFile->getTokens();
+
+        if ($tokens[$stackPtr]['content'] !== 'is_a') {
+            return;
+        }
+
+        $classnamePosition = $phpcsFile->findNext(T_CONSTANT_ENCAPSED_STRING, $phpcsFile->findNext(T_COMMA, $stackPtr));
+        if ($classnamePosition === false) {
+            return;
+        }
+
+        $classname = $tokens[$classnamePosition]['content'];
+        $this->originalTokenContent = $tokens[$classnamePosition]['content'];
+        $this->addFixableError($phpcsFile, $classnamePosition, $classname);
+    }
+
+    /**
+     * As token contains more then just class name, we have to build new content ourself.
+     *
+     * @param string $classname
+     * @return string
+     */
+    protected function getTokenForReplacement($classname)
+    {
+        return $this->getTokenReplacementForString($classname);
+    }
+}
